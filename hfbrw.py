bc2a97a914d23f01bf578b159db3220b5c5cf144
Liz Balbuena <ekevoo@gmail.com>
2015-07-15 15:01:12 -0300

    just some renames for clarity

--- hfbrw.py
+++ hfbrw.py
@@ -6,15 +6,15 @@ from os.path import dirname, abspath, join
 from hashlib import sha512
 
 
-def do_backup(target):
-    backup_dir = dirname(abspath(target))
-    hash_file = join(backup_dir, 'last_hash')
-    new_hash = sha512(open(target, 'rb').read()).digest()
-    if new_hash != open(hash_file, 'rb').read():
+def do_backup(target_path):
+    backup_dir = dirname(abspath(target_path))
+    hash_path = join(backup_dir, 'last_hash')
+    new_hash = sha512(open(target_path, 'rb').read()).digest()
+    if new_hash != open(hash_path, 'rb').read():
         fmt = '%Y%m%d-%H%M.sqlite3.bz2'
-        snapshot_file = join(backup_dir, datetime.now().strftime(fmt))
-        BZ2File(snapshot_file, 'wb').write(open(target, 'rb').read())
-        open(hash_file, 'wb').write(new_hash)
+        snapshot_path = join(backup_dir, datetime.now().strftime(fmt))
+        BZ2File(snapshot_path, 'wb').write(open(target_path, 'rb').read())
+        open(hash_path, 'wb').write(new_hash)
 
 
 if __name__ == '__main__':

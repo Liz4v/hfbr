fefaebde776075c09b1b1faac57a5d9b20735e20
Liz Balbuena <ekevoo@gmail.com>
2015-07-15 15:39:49 -0300

    Replaced full in-memory file reads with block-by-block read/write.

--- hfbrw.py
+++ hfbrw.py
@@ -16,17 +16,26 @@ def backup_target_database(target_path, backup_dir=None):
     if not backup_dir:
         backup_dir = dirname(abspath(target_path))
     hash_path = join(backup_dir, 'last_hash')
+    hasher = sha512()
     with open(target_path, 'rb') as target:
-        new_hash = sha512(target.read()).digest()
+        block_transfer(target.read, hasher.update)
     with open(hash_path, 'rb') as hashfile:
         old_hash = hashfile.read()
-    if new_hash != old_hash:
+    if hasher.digest() != old_hash:
         snapshot_filename = datetime.now().strftime('%Y%m%d-%H%M') + grab_extension(target_path) + '.bz2'
         snapshot_path = join(backup_dir, snapshot_filename)
         with open(target_path, 'rb') as target, BZ2File(snapshot_path, 'wb') as snapshot:
-            snapshot.write(target.read())
+            block_transfer(target.read, snapshot.write)
         with open(hash_path, 'wb') as hashfile:
-            hashfile.write(new_hash)
+            hashfile.write(hasher.digest())
+
+
+def block_transfer(fread, fwrite, length=16 * 1024):
+    """Copy blocks using file-like reader and write functions, based on shutil.copyfileobj."""
+    buffer = fread(length)
+    while buffer:
+        fwrite(buffer)
+        buffer = fread(length)
 
 
 def grab_extension(filename_or_path):

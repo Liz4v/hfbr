050e32b79c346acf4cc4cf9721e678084ab6c514
Liz Balbuena <ekevoo@gmail.com>
2015-07-15 22:36:11 -0300

    Added retention code.

--- hfbrw.py
+++ hfbrw.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 # coding=utf-8
 from bz2 import BZ2File
-from datetime import datetime
+from datetime import datetime, timedelta
+from functools import reduce
 from os import listdir
 from os.path import abspath, dirname, getmtime, join, sep
 from hashlib import sha512
@@ -52,17 +53,21 @@ def grab_extension(filename_or_path):
 
 class RetentionPlan:
     def __init__(self, plan_description=((None, None),)):
-        transposed_plan = [tuple(x) for x in zip(*plan_description)]
-        self.granularity, self.retention = transposed_plan
+        self.plan = plan_description
 
     def prune(self, target_dir='.', pinned_list=()):
-        files = [FileInfo(target_dir, f, pinned_list, self)
-                 for f in listdir(target_dir) if f.endswith('.bz2')]
-        files.sort(key=lambda i: -i.timestamp)  # Most recent to oldest.
+        files = [FileInfo(target_dir, f, pinned_list) for f in listdir(target_dir) if f.endswith('.bz2')]
+        for retention in [SlotOfRetention(*slot) for slot in self.plan]:
+            retention.muster(files)
+        files.sort(key=lambda f: -f.timestamp)
+        for file in files:
+            print(file)
+            # if not file.pinned:
+            #    unlink(file.filename)
 
 
 class FileInfo(object):
-    def __init__(self, dirpath, filename, pinned_list, plan):
+    def __init__(self, dirpath, filename, pinned_list):
         self.dirpath = dirpath
         self.filename = join(dirpath, filename)
         self.timestamp = getmtime(self.filename)
@@ -71,6 +76,55 @@ class FileInfo(object):
 
     def __str__(self):
         return '<%s %s %s>' % ('##' if self.pinned else '--', self.when.strftime('%Y%m%d%H%M%S'), self.filename)
+
+    def reduce(self, them):
+        if self.pinned != them.pinned:
+            return self if self.pinned > them.pinned else them
+        else:
+            return self if self.when <= them.pinned else them
+
+
+class SlotOfRetention:
+    def __init__(self, granularity, quantity):
+        if granularity is None:
+            self.granularity = 1
+            self._calc = self._calc_secdiv
+        elif isinstance(granularity, str):
+            self.granularity = granularity
+            self._calc = getattr(self, '_calc_' + granularity)
+        elif isinstance(granularity, timedelta):
+            self.granularity = int(granularity.total_seconds())
+            self._calc = self._calc_secdiv
+        else:
+            raise ValueError('Unknown granularity type %s', type(granularity))
+        self.quantity = quantity
+        self.timeslots = {}
+
+    def muster(self, list_of_files):
+        for f in list_of_files:
+            self.calc(f)
+        q = 0
+        for slot in sorted(self.timeslots.keys(), reverse=True):
+            chosen = reduce(FileInfo.reduce, self.timeslots[slot])
+            chosen.pinned = True
+            q += 1
+            if q >= self.quantity:
+                break
+
+    def calc(self, fileinfo: FileInfo):
+        position = self._calc(fileinfo)
+        if position not in self.timeslots:
+            self.timeslots[position] = []
+        self.timeslots[position].append(fileinfo)
+
+    def _calc_secdiv(self, fileinfo: FileInfo):
+        return int(fileinfo.timestamp / self.granularity)
+
+    def _calc_month(self, fileinfo: FileInfo):
+        return int(fileinfo.when.year * 12 + fileinfo.when.month)
+
+    def _calc_year(self, fileinfo: FileInfo):
+        return fileinfo.when.year
 
 
 if __name__ == '__main__':
